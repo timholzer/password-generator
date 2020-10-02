50d1ca80ddeede26a00d10ca7cb42dee1eff68cb
Tim Holzer <timiholzer@gmail.com>
2020-10-01 21:09:07 -0600

    added character type confirms

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", ];
 
 //add other variables here
-
-
-
+var selectBig = "false";
+var selectSmall = "false";
+var selectNum = "false";
+var selectSpecial = "false";
 
 //on button click - prompt "how long do you want your password to be? Has to be between 8-128 characters"
+
 document.getElementById("generate").addEventListener("click", function(){
-  var passLength = prompt("how long do you want your password to be? Has to be between 8-128 characters");
-  console.log(passLength); 
+  var passPrompt = prompt("how long do you want your password to be? Has to be between 8-128 characters");
+  console.log(passPrompt); 
+//parseInt prompt to passLength
+  var passLength = parseInt(passPrompt);
+  console.log(passLength);
+//if passLength is between 8-128 then move on, if not ask prompt again
+//if (passLength >= 129 || passLength <= 7) {
+  //need to figure out this part of the code first.
+//}
+//need to fix this loop
+while (selectBig === "false" && selectSmall === "false" && selectNum === "false" && selectSpecial === "false") {
+  selectBig = confirm("Do you want uppercase letters?");
+  selectSmall = confirm("Do you want lowercase letters?");
+  selectNum = confirm("Do you want numbers?");
+  selectSpecial = confirm("Do you want special characters?");
+  console.log(selectBig);
+  console.log(selectSmall);
+  console.log(selectNum);
+  console.log(selectSpecial);
+}
 });
 
-//parseInt prompt to passLength
-//if passLength is between 8-128 then move on, if not ask prompt again
+
+
 //confirm "do you want upper case, lower, etc"
-//if no confirms then alert "no options were checked, try again" then loop back to the confirms
+//if no confirms then loop back to the confirms
+
+
+
+
 //have a var array that has the arrays for the selected password types
 //var array length
 //randint of array -1 to select the input and loop through passlength
@@ -29,13 +53,13 @@ document.getElementById("generate").addEventListener("click", function(){
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
