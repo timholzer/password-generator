50167f2e76f7d294fc6d4db90825075ae5a29dd7
Tim Holzer <timiholzer@gmail.com>
2020-10-01 20:29:26 -0600

    added arrays for characters and password length prompt

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 //array of all upper/lower/number/special
+var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var smallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", ];
+
+//add other variables here
+
+
+
+
 //on button click - prompt "how long do you want your password to be? Has to be between 8-128 characters"
+document.getElementById("generate").addEventListener("click", function(){
+  var passLength = prompt("how long do you want your password to be? Has to be between 8-128 characters");
+  console.log(passLength); 
+});
+
 //parseInt prompt to passLength
 //if passLength is between 8-128 then move on, if not ask prompt again
 //confirm "do you want upper case, lower, etc"
