25995bc12d60d3433d8e3e0ced33d55f6392c602
Tim Holzer <timiholzer@gmail.com>
2020-10-01 20:12:25 -0600

    added psudocode for the javascript planned

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//array of all upper/lower/number/special
+//on button click - prompt "how long do you want your password to be? Has to be between 8-128 characters"
+//parseInt prompt to passLength
+//if passLength is between 8-128 then move on, if not ask prompt again
+//confirm "do you want upper case, lower, etc"
+//if no confirms then alert "no options were checked, try again" then loop back to the confirms
+//have a var array that has the arrays for the selected password types
+//var array length
+//randint of array -1 to select the input and loop through passlength
+//return string
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
